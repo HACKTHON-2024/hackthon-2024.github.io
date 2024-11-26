c37c317f53a73e03dab531955d19349aba026502
Melvin Jones P <melvinjones502@gmail.com>
2024-11-26 22:29:06 +0530

    git commit

--- frontend/Landowner/Register/script.js
+++ frontend/Landowner/Register/script.js
@@ -1,5 +1,18 @@
 window.addEventListener('online', handleNetworkChange);
 window.addEventListener('offline', handleNetworkChange);
+function togglePassword(id) {
+    const passwordField = document.getElementById(id);
+    const eyeIcon = document.getElementById(`eye-icon-${id}`);
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        eyeIcon.classList.remove("fa-eye");
+        eyeIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordField.type = "password";
+        eyeIcon.classList.remove("fa-eye-slash");
+        eyeIcon.classList.add("fa-eye");
+    }
+}
 async function onclickconfirm() {
     try {
         if (!navigator.onLine) {
